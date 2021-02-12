a8971327220adc30265e9b9e1d26aab1c7352539
mayu-f121 <78572384+mayu-f121@users.noreply.github.com>
2021-02-12 11:17:14 +0900

    データ型・文字列クラス
    課題提出確認お願いします

---
+++ java study/task1_1.java
@@ -0,0 +1,21 @@
+public class task1_1{
+
+    public static void main(String[] args) {
+        
+        int hogeInt = 50;
+        String hogeString = "テスト" ;
+
+//(”④浮動小数点型をhogeDubleと言う変数を宣言して値「0.2」で初期化”)
+        double hogeDouble = 0.2;
+
+        hogeInt = 100;
+        hogeString = "合格";
+
+            System.out.println(hogeInt);
+            System.out.println(hogeString);
+            System.out.println(hogeDouble);
+
+//("hogeStringを表示する")
+
+    }
+}
