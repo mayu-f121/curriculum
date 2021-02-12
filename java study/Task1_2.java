4ff43c4ccf660e5543b87d1dfa3bab5f05598eba
mayu-f121 <78572384+mayu-f121@users.noreply.github.com>
2021-02-12 12:08:18 +0900

    課題２
    配列について

---
+++ java study/Task1_2.java
@@ -0,0 +1,21 @@
+public class Task1_2 {
+
+    public static void main(String[] args) {
+        String [] country = {"JAPAN","AMERICA","KOREA"}; //countryの宣言
+
+        System.out.println(country.length); //要素数を出力
+
+        String [] strArray ={"りんご", "もも" , "ぶどう"};
+
+        System.out.println(strArray[1]);
+
+        //⑤int型の変数intArrayに５個の配列を入れている
+
+        int[] intArray ={10, 20, 30, 40, 50 };
+
+        //⑥intArrayの配列の中の１と、４の要素を足して出力している
+
+        System.out.println(intArray[1] + intArray[4]);
+        
+    }
+}
