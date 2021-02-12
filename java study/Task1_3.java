689d8790ea673a68afccdbd4fd37d0298d16d76e
mayu-f121 <78572384+mayu-f121@users.noreply.github.com>
2021-02-12 14:19:00 +0900

    課題３
    課題提出

---
+++ java study/Task1_3.java
@@ -0,0 +1,26 @@
+public class Task1_3 {
+
+    public static void main(String[] args){
+
+    int plus = 5 + 10;
+    int minus =20 - 7;
+
+    //③イント型１０を２で乗算して変数「multiply」に代入
+    int multiply = 10 * 2;
+
+    int remi = 20%6;
+
+    //⑤String型「有楽町」を変数「town」に代入 「線」を変数「line」に代入
+
+    String town = "有楽町";
+    String line = "線";
+    
+    String train = town + line;
+
+    System.out.println(plus);
+    System.out.println(minus);
+    System.out.println(multiply);
+    System.out.println(remi);
+    System.out.println(train);
+    }
+}
